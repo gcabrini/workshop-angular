af2729055f195be5f464f9fc2edb4cde9a265237
gcabrini <gcabrini.silva@gmail.com>
2015-09-17 17:25:08 -0300

    Remocao de exemplos antigos.

--- exemplos/exemplo-7/src/app/commons/utils.js
+++
@@ -1,7 +0,0 @@
-(function(ng) {
-    ng.module('app')
-        .constant('Utils', {
-          "localUrl": "http://localhost:3000",
-          "restUrl": "http://172.21.186.43:3000"
-    });
-})(angular);
